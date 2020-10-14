5a8711b61251c9745e6f7a00f8395e8922c4d94e
josepbernal23 <josepbernal23@gmail.com>
2020-10-14 20:20:15 +0200

    Versió 0.1 del projecte prjava00

---
+++ prjava00.java
@@ -0,0 +1,8 @@
+public class prjava00 {
+    public static void main(String[] args) {
+        System.out.println("Hola món");
+        System.out.println("Versió 0.1 del projecte prjava00");
+    }
+}
+
+//gadfgadfgadfgdagadg
